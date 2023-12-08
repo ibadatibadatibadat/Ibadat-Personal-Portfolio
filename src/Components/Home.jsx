e37f0a9214fbbf2d32a151e9c092e43b75e0c8bf
ibadatibadatibadat <ibadatafridi7@gmail.com>
2023-12-08 20:25:55 +0500

    Creating A Rough Skeleton for our Home Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import "../styles/home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="main">
+      <div className="div1">
+        <h1>IT'S ME</h1>
+        <p>
+          <i>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            tempora, doloremque soluta iure est pariatur! Autem, in quos qui
+            facilis aliquam fuga, nostrum cupiditate at quasi dolorem
+            dignissimos asperiores libero neque animi ducimus fugit obcaecati
+            molestias! Distinctio quaerat aliquam aliquid.
+          </i>
+        </p>
+      </div>
+      <div className="line"></div>
+      <div className="div2">
+        <h1>IT'S ME</h1>
+        <p>
+          <i>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            tempora, doloremque soluta iure est pariatur! Autem, in quos qui
+            facilis aliquam fuga, nostrum cupiditate at quasi dolorem
+            dignissimos asperiores libero neque animi ducimus fugit obcaecati
+            molestias! Distinctio quaerat aliquam aliquid.
+          </i>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
