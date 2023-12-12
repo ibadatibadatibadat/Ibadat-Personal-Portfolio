b51f8c16323bcc26a5c38e1a87cbe87b2128149d
ibadatibadatibadat <ibadatafridi7@gmail.com>
2023-12-12 17:26:15 +0500

    Styling Skills Componet

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
+import "../styles/services.css";
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="main">
+      <div className="card">
+        <img src="./vite.svg" alt="card-photo" className="card-cover" />
+        <div className="card-item">
+          <h3>Web Development</h3>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            distinctio iste, corrupti omnis repellat quos fugiat rerum
+            consectetur nisi totam!
+          </p>
+          <button className="more">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <img src="./vite.svg" alt="card-photo" className="card-cover" />
+        <div className="card-item">
+          <h3>Web Development</h3>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            distinctio iste, corrupti omnis repellat quos fugiat rerum
+            consectetur nisi totam!
+          </p>
+          <button className="more">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <img src="./vite.svg" alt="card-photo" className="card-cover" />
+        <div className="card-item">
+          <h3>Web Development</h3>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            distinctio iste, corrupti omnis repellat quos fugiat rerum
+            consectetur nisi totam!
+          </p>
+          <button className="more">More Info</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
