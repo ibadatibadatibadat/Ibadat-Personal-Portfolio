f813612257ac3de61e4380ee3a66cfdaa207963e
ibadatibadatibadat <ibadatafridi7@gmail.com>
2023-12-12 16:41:53 +0500

    Styling and Creating Services Components

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,7 +1,46 @@
 import React from "react";
-
+import "../styles/services.css";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="main">
+      <div className="card">
+        <img src="./vite.svg" alt="card-photo" className="card-cover" />
+        <div className="card-item">
+          <h3>Free Lancing</h3>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            distinctio iste, corrupti omnis repellat quos fugiat rerum
+            consectetur nisi totam!
+          </p>
+          <button className="more">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <img src="./vite.svg" alt="card-photo" className="card-cover" />
+        <div className="card-item">
+          <h3>Free Lancing</h3>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            distinctio iste, corrupti omnis repellat quos fugiat rerum
+            consectetur nisi totam!
+          </p>
+          <button className="more">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <img src="./vite.svg" alt="card-photo" className="card-cover" />
+        <div className="card-item">
+          <h3>Free Lancing</h3>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            distinctio iste, corrupti omnis repellat quos fugiat rerum
+            consectetur nisi totam!
+          </p>
+          <button className="more">More Info</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
