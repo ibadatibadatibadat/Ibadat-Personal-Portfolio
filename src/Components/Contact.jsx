32d7f4843a11a94a508ba8b0e6779c0b74aaed8a
ibadatibadatibadat <ibadatafridi7@gmail.com>
2023-12-13 19:54:13 +0500

    Creating Form For Contact Page And Styling Form+Contact Page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,77 @@ import React from "react";
 import "../styles/contact.css";
 
 const Contact = () => {
-  return <div className="main">Contact</div>;
+  return (
+    <div className="main">
+      <h1 className="heading">Contact Us</h1>
+      <div className="form-container">
+        <form action="post">
+          <h3 className="form-heading">Contact From</h3>
+          <div className="input-container">
+            <h4 className="input-heading">
+              First Name: <span className="star-item">*</span>{" "}
+            </h4>
+            <input
+              className="input-field"
+              type="text"
+              placeholder="enter your first name"
+            />
+          </div>
+          <div className="input-container">
+            <h4 className="input-heading">
+              Last Name: <span className="star-item">*</span>{" "}
+            </h4>
+            <input
+              className="input-field"
+              type="text"
+              placeholder="enter your Last name"
+            />
+          </div>
+          <div className="input-container">
+            <h4 className="input-heading">
+              Email : <span className="star-item">*</span>{" "}
+            </h4>
+            <input
+              className="input-field"
+              type="email"
+              placeholder="enter your email"
+            />
+          </div>
+          <div className="input-container">
+            <h4 className="input-heading">
+              Contact No.: <span className="star-item">*</span>{" "}
+            </h4>
+            <input
+              className="input-field"
+              type="number"
+              placeholder="enter your Contact Number"
+            />
+          </div>
+          <div className="input-container">
+            <h4 className="input-heading">
+              Feedback: <span className="star-item">*</span>{" "}
+            </h4>
+            <input
+              className="input-field"
+              type="text"
+              placeholder="enter your Feedback"
+            />
+          </div>
+          <div className="input-container">
+            <h4 className="input-heading">
+              Social Media: <span className="star-item">*</span>{" "}
+            </h4>
+            <input
+              className="input-field"
+              type="text"
+              placeholder="enter your Social Media Id"
+            />
+          </div>
+          <button type="submit" className="submit-form">sumbit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
